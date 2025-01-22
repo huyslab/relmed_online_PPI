65de4194daa7111c4027d91058d6eaeb8dacf0c6
yanivabir <yanivabir@gmail.com>
2025-01-22 14:54:07 +0000

    long deadline duration

--- PILT.js
+++ PILT.js
@@ -1,5 +1,5 @@
 // Function setting up PILT blocks
-window.default_response_deadline = 4000;
+window.default_response_deadline = 1000;
 window.pilt_test_confidence_every = 4;
 
 window.skipThisBlock = false;
